3e146c66c4fdc2bc3d0843295e5191ee5e6fdac1
Arafin Mridul <mridularafin@gmail.com>
2024-01-10 18:44:28 +0600

    probs written in json

--- index.js
+++ index.js
@@ -2,31 +2,62 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 async function run() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.amazon.com/gpu/s?k=gpu");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+        "https://cp-algorithms.com/graph/breadth-first-search.html"
+    );
 
-  // Get a screenshot of the page
-  await page.screenshot({ path: "gpu.png", fullPage: true });
+    // screenshot of the page
+    await page.screenshot({ path: "cpalgos.png", fullPage: true });
 
-  // Get a PDF of the page
-  await page.pdf({ path: "gpu.pdf", format: "A4" });
+    // pdf of the page
+    await page.pdf({ path: "cpalgos.pdf", format: "A4" });
 
-  //  Get HTML of the page
-  const html = await page.content();
+    //  page content
+    const html = await page.content();
 
-  //  Get text of the page
-  const title = await page.evaluate(() => document.title);
+    //  page title
+    const title = await page.evaluate(() => document.title);
 
-  //  Get text of the page
-  const text = await page.evaluate(() => document.body.innerText);
+    //  text of the page
+    const text = await page.evaluate(() => document.body.innerText);
 
-  //  Get all links
-  const links = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("a"), (e) => e.href)
-  );
+    //  embedded links
+    const links = await page.evaluate(() =>
+        Array.from(document.querySelectorAll("a"), (e) => e.href)
+    );
 
-  await browser.close();
+    // extracting the html to a file
+    // try {
+    //     fs.writeFileSync("cpalgos.html", html);
+    //     console.log("html file has been saved!");
+    // } catch (err) {
+    //     console.log(err);
+    // }
+
+    // get cp problems
+    // const problems = await page.evaluate(() =>
+    //     Array.from(document.querySelectorAll(".md-typeset ul li a"), (e) => ({
+    //         problem: e.innerText,
+    //         url: e.href,
+    //     }))
+    // );
+
+    const problems = await page.$$eval(".md-typeset ul li a", (elements) =>
+        elements.map((item) => ({
+            problem: item.innerText,
+            url: item.href,
+        }))
+    );
+
+    // save problems to a JSON file
+    fs.writeFile("cpalgos.json", JSON.stringify(problems), (err) => {
+        if (err) throw err;
+        console.log("problems file has been saved!");
+    });
+
+    await browser.close();
 }
 
 run();
